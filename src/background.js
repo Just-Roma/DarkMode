2cfca5300bdf17ea097da2aec192ef6c8be4d464
Roman Swietlicki <64587275+Just-Roma@users.noreply.github.com>
2023-10-22 17:49:06 +0200

    Update background.js
    Fix bugs: CSS was inserted multiple times through chrome.scripting.insertCSS. The events "onActivated" and "onClicked" did not work properly.

--- src/background.js
+++ src/background.js
@@ -1,3 +1,7 @@
+/*
+All errors are logged with console.log to avoid popping up of the "errors" button.
+*/
+
 'use strict'
 
 const DARK_PATH = {
@@ -16,7 +20,6 @@ const LIGHT_PATH = {
   512: '/icons/light_512.png'
 }
 
-
 function reload_page(id, event){
   chrome.tabs.reload(
     id,
@@ -27,14 +30,32 @@ function reload_page(id, event){
   });
 }
 
+function reload_http_and_https(id, event){
+  chrome.tabs.query({currentWindow: true, active: true}).then(query_result => {
+    if ('url' in query_result[0] && query_result[0].url.match(/^https?/)){
+      reload_page(id, event);
+    }
+  }, error => {
+    console.log(`${event}: failed to query the active tab: ${error}`);
+  });
+}
+
 function insert_CSS(id, event){
   chrome.scripting.insertCSS({
     target: {
       tabId: id,
     },
-    files: ['src/rules.css'],
+    files: ['/src/rules.css'],
   }).
-  catch (error => {
+  then(() => {
+    chrome.storage.session.set({
+      [String(id)]: {
+        'page_is_dark': false,
+        'css_inserted': true
+      }
+    }).
+    catch(error => {console.log(`${event}: failed to store tab's settings: ${error}`);});
+  }, error => {
     console.log(`${event}: failed to insert CSS: ${error}`);
   });
 }
@@ -44,51 +65,19 @@ function remove_CSS(id, event){
     target: {
       tabId: id,
     },
-    files: ['src/rules.css'],
+    files: ['/src/rules.css'],
   }).
-  catch (error => {
+  then(() => {
+    chrome.storage.session.set({
+      [String(id)]: {
+        'page_is_dark': false,
+        'css_inserted': false
+      }
+    }).
+    catch(error => {console.log(`${event}: failed to store tab's settings: ${error}`);});
+  }, error => {
     console.log(`${event}: failed to remove CSS: ${error}`);
   });
-}
-
-function handle_CSS(id, event, page_is_dark, extension_mode){
-  if (extension_mode == 'dark'){
-    /* We only need to handle the case, when page is light by default.
-    */
-    if (page_is_dark === false){
-      insert_CSS(id, event);
-    }
-    else{
-      // Page is dark by default. Do nothing.
-    }
-  }
-  else if (extension_mode == 'light'){
-    /* We only need to handle the case, when page is light by default.
-    */
-    if (page_is_dark === false){
-      remove_CSS(id, event);
-    }
-    else{
-      // Page is dark by default. Do nothing.
-    }
-  }
-  // Shall never happen, but to be 100% sure.
-  else{
-    console.log('handle_CSS: unrecognized extension mode.');
-  }
-}
-
-function handle_CSS_on_event(id, event, page_is_dark, mode = null){
-  if (mode === null){
-    chrome.storage.local.get(['mode']).then(results => {
-      handle_CSS(id, event, page_is_dark, results['mode']);
-    }, error => {
-      console.log(`handle_CSS_on_event: failed to get extension's mode: ${error}`);
-    });
-  }
-  else{
-    handle_CSS(id, event, page_is_dark, mode);
-  }
 }
 
 
@@ -123,7 +112,7 @@ chrome.runtime.onStartup.addListener(() => {
         console.log(`onStartup: failed to set title for light mode: ${error}`);
       });
     }
-    // Shall never happen, but to be 100% sure.
+    // Shall never happen, but print just in case.
     else{
       console.log('onStartup: unrecognized mode.');
     }
@@ -137,12 +126,15 @@ chrome.action.onClicked.addListener(tab => {
   /* Clicking on the extension button shall reset the mode.
   */
   const tabId = String(tab.id);
-  chrome.storage.local.get(['mode']).then(results => {
-    if (results['mode'] == 'dark'){
+  chrome.storage.local.get(['mode']).then(mode => {
+    if (mode['mode'] == 'dark'){
       chrome.storage.local.set({ mode: 'light' }).then(() => {
         chrome.storage.session.get([tabId]).then(tab_settings => {
+          /* If tab is stored, then set the light mode.
+          */
           if (tabId in tab_settings){
-            if (tab_settings[tabId]['page_is_dark'] === false){
+            tab_settings = tab_settings[tabId];
+            if (tab_settings['page_is_dark'] === false && tab_settings['css_inserted'] == true){
               remove_CSS(tab.id, 'onClicked');
             }
             else{
@@ -150,10 +142,10 @@ chrome.action.onClicked.addListener(tab => {
             }
           }
           else{
-            reload_page(tab.id, 'onClicked');
+            reload_http_and_https(tab.id, 'onClicked');
           }
         }, error => {
-          console.log(`onClicked: failed to get tab's ID: ${error}`);
+          console.log(`onClicked: failed to get tab's settings: ${error}`);
         });
         chrome.action.setIcon({ path: LIGHT_PATH }).catch(error => {
           console.log(`onClicked: failed to set light icon: ${error}`);
@@ -165,11 +157,14 @@ chrome.action.onClicked.addListener(tab => {
         console.log(`onClicked: failed to set light mode: ${error}`);
       });
     }
-    else if (results['mode'] == 'light'){
+    else if (mode['mode'] == 'light'){
       chrome.storage.local.set({ mode: 'dark' }).then(() => {
         chrome.storage.session.get([tabId]).then(tab_settings => {
+          /* If tab is stored, then set the dark mode.
+          */
           if (tabId in tab_settings){
-            if (tab_settings[tabId]['page_is_dark'] === false){
+            tab_settings = tab_settings[tabId];
+            if (tab_settings['page_is_dark'] === false && tab_settings['css_inserted'] == false){
               insert_CSS(tab.id, 'onClicked');
             }
             else{
@@ -177,10 +172,10 @@ chrome.action.onClicked.addListener(tab => {
             }
           }
           else{
-            reload_page(tab.id, 'onClicked');
+            reload_http_and_https(tab.id, 'onClicked');
           }
         }, error => {
-        console.log(`onClicked: failed to get tab's ID: ${error}`);
+        console.log(`onClicked: failed to get tab's settings: ${error}`);
         });
         chrome.action.setIcon({ path: DARK_PATH }).catch(error => {
           console.log(`onClicked: failed to set dark icon: ${error}`);
@@ -192,36 +187,73 @@ chrome.action.onClicked.addListener(tab => {
         console.log(`onClicked: failed to set dark mode: ${error}`);
       });
     }
-    // Shall never happen, but to be 100% sure.
+    // Shall never happen, but print just in case.
     else{
-      console.log('onClicked: failed to get mode.');
+      console.log('onClicked: wrong mode.');
     }
   }, error => {
-    console.log(`onClicked: failed to get mode event: ${error}`);
+    console.log(`onClicked: failed to get mode: ${error}`);
   });
 });
 
 
 chrome.tabs.onActivated.addListener(tab => {
+  /* The first click on a tab shall put it in appropriate mode.
+  */
   const tabId = String(tab.tabId);
-  chrome.storage.local.get(['mode']).then(local_settings => {
-    chrome.storage.session.get([tabId]).then(tab_settings => {
-      if (tabId in tab_settings){
-        if (local_settings['mode'] == 'dark' && tab_settings[tabId]['page_is_dark'] === false){
-          insert_CSS(tab.tabId, 'onActivated');
-        }
-        else if (local_settings['mode'] == 'light' && tab_settings[tabId]['page_is_dark'] === false){
-          remove_CSS(tab.tabId, 'onActivated');
+  chrome.storage.local.get(['mode']).then(mode => {
+    chrome.storage.session.get([tabId]).then(result => {
+      /* If tab is stored, then apply the right mode.
+      */
+      if (tabId in result){
+        const tab_settings = result[tabId];
+        if (tab_settings['page_is_dark'] === false){
+          if (mode['mode'] == 'dark'){
+            if (tab_settings['css_inserted'] == false){
+              insert_CSS(tab.tabId, 'onActivated');
+            }
+            else{
+              // Mode is dark and CSS is already inserted. Do nothing.
+            }
+          }
+          else if (mode['mode'] == 'light'){
+            if (tab_settings['css_inserted'] == true){
+              remove_CSS(tab.tabId, 'onActivated');
+            }
+            else{
+              // Mode is light and CSS is already removed. Do nothing.
+            }
+          }
+          // Shall never happen, but print just in case.
+          else{
+            console.log('onActivated: wrong mode.');
+          }
         }
         else{
-
+          // Page is dark by default. Do nothing.
         }
       }
+      /* It can happen that a tab was not stored. Eg already existing tabs after installation.
+      */
       else{
-        reload_page(tab.tabId, 'onActivated');
+        /* It can happen that content scripts are not injected in a page.
+        Eg www.google.com in Opera. Maybe it is necessary to adjust browser's
+        configurations to make it work. But it probably requires manual work:
+        In the ExtensionsSettings for instance. So, if a tab, after being
+        activated, does not have an entry in the session storage, it gets one.
+        This means that the tab won't be reloaded each time it is clicked on.
+        */
+        chrome.storage.session.set({
+          [tabId]: {}
+        }).
+        then(() => {
+          reload_http_and_https(tab.tabId, 'onActivated');
+        }, error => {
+          console.log(`onActivated: failed to store tab: ${error}`);
+        });
       }
     }, error => {
-      console.log(`onActivated: failed to get tab's ID: ${error}`);
+      console.log(`onActivated: failed to get tab's settings: ${error}`);
     });
   }, error => {
     console.log(`onActivated: failed to get mode: ${error}`);
@@ -230,6 +262,8 @@ chrome.tabs.onActivated.addListener(tab => {
 
 
 chrome.tabs.onRemoved.addListener(tabId => {
+  /* Remove tab's entry from the session storage after closing it.
+  */
   chrome.storage.session.remove([String(tabId)]).catch(error => {
     console.log(`onRemoved: failed to remove tab: ${error}`);
   });
@@ -237,18 +271,39 @@ chrome.tabs.onRemoved.addListener(tabId => {
 
 
 chrome.runtime.onMessage.addListener((data, sender) => {
+  /* React on a message coming from content script.
+  */
   const tabId = sender.tab.id;
   const tabIdStr = String(tabId);
-  const page_is_dark = data.page_is_dark;
-  //chrome.storage.session.get([tabIdStr]).then(tab_settings => {
-  //  console.log('onmessage ', tab_settings);
-  chrome.storage.session.set({
-    [tabIdStr]: {
-      'page_is_dark': page_is_dark}
-  }).
-  then(() => {
-    handle_CSS_on_event(tabId, 'onMessage', page_is_dark);
-  }, error => {
-    console.log(`onMessage: failed to store tab's ID: ${error}`);
-  });
+  /*
+  Change only if page is light.
+  */
+  if (data.page_is_dark === false){
+    chrome.storage.local.get(['mode']).then(mode => {
+      if (mode['mode'] == 'dark'){
+        insert_CSS(tabId, 'onMessage');
+      }
+      else if (mode['mode'] == 'light'){
+        chrome.storage.session.set({
+          [tabIdStr]: {
+            'page_is_dark': false,
+            'css_inserted': false
+          }
+        }).catch(error => {console.log(`onMessage: failed to store tab's settings: ${error}`);});
+      }
+      // Shall never happen, but print just in case.
+      else{
+        console.log('onMessage: unrecognized mode.');
+      }
+    }, error => {
+      console.log(`onMessage: failed to get mode: ${error}`);
+    });
+  }
+  // Otherwise no css will ever be needed.
+  else{
+    chrome.storage.session.set({
+        [tabIdStr]: {'page_is_dark': true}
+    }).
+    catch(error => {console.log(`onMessage: failed to store tab's settings: ${error}`);});
+  }
 });
